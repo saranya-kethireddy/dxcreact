d7a56635f600348da6e088b5d3d354f2c45fce52
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-08 16:00:54 +0530

    handling state change

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
 
  
 
 function App() {
   return (
     <div className="App">
+
+      <Message/>
       <Greet name = "saranya"  college = "IIT">
       <p> this is the children property</p>
     </Greet>
-    <Greet name = "Amar" college = "Oxford"/>
-    <Greet name ="reddy"/>
+    <Greet name = "Amar" college = "Oxford">
+    <button>click me</button>
+    </Greet>
+    <Greet />
 
-    <Welcome/>
+    
+
+    <Welcome name ="amar" alias = "dasuru"/>
 
     </div>
   );
