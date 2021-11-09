488825bfa396498f4789d4abc1d4516c775eca41
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-09 17:23:00 +0530

    lists using map

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 function NameList() {
-    const names = ['saranya','reddy','kethireddy']
+    const names = ['saranya','reddy','kethireddy','dxc']
     return (
         <div>
-            <h2>{names[0]}</h2>
-            <h2>{names[1]}</h2>
-            <h2>{names[2]}</h2>
+           {
+               names.map(name => <h2>{name}</h2>)
+           }
+
 
         </div>
     )
