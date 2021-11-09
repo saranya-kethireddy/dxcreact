be5eb42d1008a4ee818abf890c277064b76504ff
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-09 14:47:00 +0530

    event binding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import ClassClick from './components/ClassClick';
-import FunctionClick from './components/FunctionClick';
+
+import EventBind from './components/EventBind';
 
 
 
@@ -13,21 +9,8 @@ function App() {
   return (
     <div className="App">
 
-      <ClassClick/>
-      <FunctionClick/>
-      <Counter />
-      <Message />
-      <Greet name="saranya" college="IIT">
-        <p> this is the children property</p>
-      </Greet>
-      <Greet name="Amar" college="Oxford">
-        <button>click me</button>
-      </Greet>
-      <Greet />
-
-
-
-      <Welcome name="amar" alias="dasuru" />
+      
+      <EventBind/>
 
     </div>
   );
