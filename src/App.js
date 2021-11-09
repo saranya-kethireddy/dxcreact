4b4dd359ae580d85de14c2e4babf9bef8e7d970e
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-09 11:33:46 +0530

    event handling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import ClassClick from './components/ClassClick';
+import FunctionClick from './components/FunctionClick';
 
 
 
 function App() {
   return (
     <div className="App">
+
+      <ClassClick/>
+      <FunctionClick/>
       <Counter />
       <Message />
       <Greet name="saranya" college="IIT">
