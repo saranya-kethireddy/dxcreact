12966725a7508edd6386294d795b1e0adddeff09
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-09 16:24:23 +0530

    conditional rendering .. if/else

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
 
 
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="App">
 
+      <UserGreeting/>
       <ParentComponent/>
       <EventBind/>
 
