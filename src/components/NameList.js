147b143d9459251abeb7dd5f75ddc96f1de38367
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-09 17:39:48 +0530

    list of persons json with multiple properties

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,15 +1,44 @@
 import React from 'react'
 
 function NameList() {
-    
-           const names = ['saranya', 'reddy', 'kethireddy', 'dxc','react']
-           const namesList = names.map(name => <h2>{name}</h2>)
+
+    const persons = [
+        {
+            id: 1,
+            name: 'saranya',
+            age: 22,
+            skill: 'react'
+        },
+        {
+            id: 1,
+            name: 'amar',
+            age: 28,
+            skill: 'flutter'
+        },
+        {
+            id: 1,
+            name: 'saru',
+            age: 22,
+            skill: 'java'
+        },
+        {
+            id: 1,
+            name: 'abc',
+            age: 30,
+            skill: 'spring'
+        }
+    ]
 
 
-        
-         return (
-            <div>{namesList}</div>
+
+    /* const names = ['abdul', 'ansari', 'tanveer', 'dxc','react']
+    const namesList = names.map(name => <h2>{name}</h2>) */
+    const personList = persons.map(person => <h2> i am {person.name},i am {person.age} years old and my skill is {person.skill}</h2>)
+    return (
+
+        <div>{personList}</div>
     )
+
 }
 
 export default NameList
