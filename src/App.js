92fe6be151aa2e3117fa73cefd0840b1c860228a
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-10 12:03:42 +0530

    conditional styling

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
   return (
     <div className="App">
 
-      <Stylesheet/>
+      
+      <Stylesheet enabled = {false}/>
       <Table/>
 
       <UserGreeting/>
--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './myStyles.css'
 
-function Stylesheet() {
+function Stylesheet(props) {
+
+    let className = props.enabled ? 'primary' : ''
     return (
         <div>
-            <h1 className = 'primary'> welcome to style sheets</h1>
+            <h1 className = {className}> welcome to style sheets</h1>
         </div>
     )
 }
