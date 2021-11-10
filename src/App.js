72409d1f99394fd18b6d0e87e00afdea42136ede
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-10 12:40:26 +0530

    css modules

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ import Table from './components/Table';
 import UserGreeting from './components/UserGreeting';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 
 
 function App() {
   return (
     <div className="App">
+
+      <h1 className = {styles.success}>success</h1>
+      <h1 className = 'error'>error</h1>
 
       <Inline/>
       <Stylesheet enabled = {true}/>
