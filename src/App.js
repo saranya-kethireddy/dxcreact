6a10f9ef18f806efec9b5109df33fd77b69dc4f9
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-12 16:16:36 +0530

    forwarding refs

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import './App.css';
 
 
 import './appStyles.css'
-import styles from './appStyles.module.css'
-import Form from './components/Form';
-import FocusInput from './components/refs/FocusInput';
+
+import FrParentInput from './components/refs/FrParentInput';
 import Input from './components/refs/Input';
 
 
@@ -13,7 +12,7 @@ import Input from './components/refs/Input';
 function App() {
   return (
     <div className="App">
-      <FocusInput/>
+     <FrParentInput/>
 
     </div>
   );
