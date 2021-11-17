90d790d2381a80589e8ea5bbdb1c30d9615e459a
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-17 16:57:01 +0530

    use effect hook

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import './appStyles.css'
 import HookCounter from './components/hooks/HookCounter';
 import HookCounter2 from './components/hooks/HookCounter2';
 import HookCounter3 from './components/hooks/HookCounter3';
+import CounterHook1 from './components/hooks/useeffect/CounterHook1';
 
 
 
 function App() {
   return (
     <div className="App">
-     <HookCounter2/>
-     <HookCounter3/>
+     <CounterHook1/>
      
 
     </div>
