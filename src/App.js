ca91ba3a9e6b5482fda92d2816b080fd14afe499
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-17 13:15:10 +0530

    use state with object and spread operator

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import './appStyles.css'
 import HookCounter from './components/hooks/HookCounter';
 import HookCounter2 from './components/hooks/HookCounter2';
+import HookCounter3 from './components/hooks/HookCounter3';
 
 
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="App">
      <HookCounter2/>
+     <HookCounter3/>
      
 
     </div>
